82530acd07ec062fcdce98cb8f7cd663af86dfcc
郑皓沥 <zhenghaoli1996@gmail.com>
2020-08-12 17:21:11 +0800

    feat: 选择got作为请求库

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const Koa = require('koa');
 const Router = require('koa-route');
-const app = new Koa();
+const bodyParser = require('koa-bodyparser')
 const {getProxy, postProxy, fileUploadProxy} = require('./src/middlewares');
 
+const app = new Koa();
+
+app.use(bodyParser());
 app.use(getProxy);
 app.use(postProxy);
 app.use(fileUploadProxy);
 
-app.use(Router.get('*', ctx => {
-    const href = ctx.request.href;
-    ctx.response.body = href;
-}))
+app.use( async ( ctx ) => {
+    if ( ctx.path === '/' && ctx.method === 'GET' ) {
+      // 当GET请求时候返回表单页面
+      let html = `
+        <h1>koa2 request post demo</h1>
+        <form method="POST" action="/">
+          <p>userName</p>
+          <input name="userName" /><br/>
+          <p>nickName</p>
+          <input name="nickName" /><br/>
+          <p>email</p>
+          <input name="email" /><br/>
+          <button type="submit">submit</button>
+        </form>
+      `
+      ctx.body = html
+    } else if ( ctx.path === '/' && ctx.method === 'POST' ) {
+      // 当POST请求的时候，中间件koa-bodyparser解析POST表单里的数据，并显示出来
+      let postData = ctx.request.body
+      ctx.body = postData
+    } else {
+      // 其他请求显示404
+      ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+    }
+  })
+  
+  
 
 app.listen(3000);
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,10 +1,24 @@
+const got = require('got');
+
 // 对get请求进行转发
 const getProxy = async (ctx, next) => {
+    if (ctx.request.method === 'GET') {
+        const href = ctx.request.href;
+        // 转发请求
+        const res = await got(href);
+        ctx.response.body = res;
+    }
     await next();
 }
 
 // 对post请求进行转发
 const postProxy = async (ctx, next) => {
+    if (ctx.request.method === 'POST') {
+        const href = ctx.request.href;
+        const postData = ctx.request.body
+        // 转发请求
+        got.post(href, {json: postData})
+    }
     await next();
 }
 
